2a891e348cc55f6fdc5de8840753ea9523968c4b
김승녕 <workingsnkim@gmail.com>
2022-04-29 17:39:08 +0900

    deploy test

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-PID=$(cat report-api.pid)
+PID=$(cat /data/app/report-api/report-api.pid)
 echo $PID
 
 if [ ! -d $PID ] ; then
         kill -9 $PID
         echo "$PID is Killed"
-        rm -rf report-api.pid
+        rm -rf /data/app/report-api/.pid
 else
         echo "Process Already Killed"
 fi
