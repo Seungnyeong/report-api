5e20ae1a989f30028dcba42c35329558d0b7fc82
김승녕 <workingsnkim@gmail.com>
2022-05-02 10:49:13 +0900

    cross origin setting from webmvc config

--- src/main/java/com/wemakeprice/vms/reportapi/config/WebMvcConfig.java
+++ src/main/java/com/wemakeprice/vms/reportapi/config/WebMvcConfig.java
@@ -5,10 +5,12 @@ import com.navercorp.lucy.security.xss.servletfilter.XssEscapeServletFilter;
 import com.wemakeprice.vms.reportapi.common.utils.xss.HTMLCharacterEscapes;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
@@ -17,6 +19,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
     private final ObjectMapper objectMapper;
+
+    @Value("${cross.origin-url}")
+    private String originUrl;
+
     @Bean
     public FilterRegistrationBean<XssEscapeServletFilter> filterRegistrationBean() {
         FilterRegistrationBean<XssEscapeServletFilter> filterRegistration = new FilterRegistrationBean<>();
@@ -33,4 +39,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
         copy.getFactory().setCharacterEscapes(new HTMLCharacterEscapes());
         return new MappingJackson2HttpMessageConverter(copy);
    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(originUrl);
+    }
 }
