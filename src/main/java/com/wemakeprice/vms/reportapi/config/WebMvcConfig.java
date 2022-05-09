a8b76157da1169589796bcf4db81b338afc08ab3
김승녕 <workingsnkim@gmail.com>
2022-05-09 18:25:15 +0900

    test

--- src/main/java/com/wemakeprice/vms/reportapi/config/WebMvcConfig.java
+++ src/main/java/com/wemakeprice/vms/reportapi/config/WebMvcConfig.java
@@ -43,6 +43,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(originUrl);
+                .allowedOrigins("*")
+                .allowedMethods("*");
     }
 }
